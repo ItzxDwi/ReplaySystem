70a837fb96e221fe152e6750946febe4fc011d5a
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2024-07-05 10:18:31 +0800

    pm5

--- src/ReplaySystem/Listener/onPlayerToggleSneak.php
+++ src/ReplaySystem/Listener/onPlayerToggleSneak.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: McpeBooster
- * Date: 07.03.2018
- * Time: 22:10
- */
 
 namespace ReplaySystem\Listener;
-
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerToggleSneakEvent;
@@ -19,10 +12,10 @@ class onPlayerToggleSneak implements Listener {
     public function onPlayerToggleSneak(PlayerToggleSneakEvent $event) {
         if (!$event->isCancelled()) {
             $entity = $event->getPlayer();
-            $level = $entity->getLevel();
-            if ($replay = ReplayManager::getActiveReplayByLevel($level)) {
+            $level = $entity->getWorld();
+            if ($replay = ReplayManager::getActiveReplayByWorld($world)) {
                 if ($replay instanceof Replay) {
-                    $replay->addEntry("Sneak", $entity->getId(), ["Sneak" => $entity->isSneaking()], ["Id" => $entity->getInventory()->getItemInHand()->getId()]);
+                    $replay->addEntry("Sneak", $entity->getId(), ["Sneak" => $entity->isSneaking()], ["Id" => $entity->getInventory()->getItemInHand()->getVanillaName()]);
                 }
             }
         }
