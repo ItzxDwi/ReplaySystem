2b956dbafec1789e42de27bd3da911f2eb20d0f3
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2024-07-05 10:16:47 +0800

    pm5

--- src/ReplaySystem/Listener/onPlayerItemConsume.php
+++ src/ReplaySystem/Listener/onPlayerItemConsume.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: McpeBooster
- * Date: 08.03.2018
- * Time: 13:02
- */
 
 namespace ReplaySystem\Listener;
-
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemConsumeEvent;
@@ -19,10 +12,10 @@ class onPlayerItemConsume implements Listener {
     public function onPlayerItemConsume(PlayerItemConsumeEvent $event){
         if (!$event->isCancelled()) {
             $entity = $event->getPlayer();
-            $level = $entity->getLevel();
-            if ($replay = ReplayManager::getActiveReplayByLevel($level)) {
+            $level = $entity->getWorld();
+            if ($replay = ReplayManager::getActiveReplayByWorld($world)) {
                 if ($replay instanceof Replay) {
-                    $replay->addEntry("Consume", $entity->getId(), null, ["Id" => $entity->getInventory()->getItemInHand()->getId()]);
+                    $replay->addEntry("Consume", $entity->getId(), null, ["Id" => $entity->getInventory()->getItemInHand()->getVanillaName())]);
                 }
             }
         }
