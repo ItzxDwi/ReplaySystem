ec3f729a28016905950dcb59d0ef92bbd733287a
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2024-07-05 10:15:28 +0800

    pm5

--- src/ReplaySystem/Listener/onPlayerDeath.php
+++ src/ReplaySystem/Listener/onPlayerDeath.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: McpeBooster
- * Date: 08.03.2018
- * Time: 14:55
- */
 
 namespace ReplaySystem\Listener;
-
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
@@ -17,12 +10,11 @@ use ReplaySystem\Manager\ReplayManager;
 class onPlayerDeath implements Listener {
 
     public function onPlayerDeath(PlayerDeathEvent $event) {
-        //var_dump("onPlayerDeath");
         $entity = $event->getEntity();
-        $level = $entity->getLevel();
-        if ($replay = ReplayManager::getActiveReplayByLevel($level)) {
+        $level = $entity->getWorld();
+        if ($replay = ReplayManager::getActiveReplayByWorld($world)) {
             if ($replay instanceof Replay) {
-                $replay->addEntry("Death", $entity->getId(), null, ["Id" => $entity->getInventory()->getItemInHand()->getId()]);
+                $replay->addEntry("Death", $entity->getId(), null, ["Id" => $entity->getInventory()->getItemInHand()->getVanillaName()]);
             }
 
         }
